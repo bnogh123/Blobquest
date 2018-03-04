fea243a45fe711f9192b199349fff8c15bcfac2d
Bassem <bassemnoghnogh11@gmail.com>
2018-03-04 11:09:43 -0600

    tilemap but not manager

--- Code/Game.py
+++ Code/Game.py
@@ -19,7 +19,6 @@ class Game:
         scale = self.world.get_backbuffer_size()
         pc.scale_to(scale)
 
-        # pokemon_center = r.TileMap(white.png)
         bulb_path = os.path.join("..//Sprites2//bulbs-1.png")
         # bulb = r.Sprite()
         # bulb.load_texture(bulb_path)
@@ -28,9 +27,32 @@ class Game:
         bulb.scale_to(r.Vector2(48, 40))
         moves = KeyboardManager(bulb)
 
+        # nurse_joy = r.TileMapManager()
+        #
+        # # The Collision map
+        # collisions = {}
+        # collisions["0"] = ["Passthrough"]
+        # collisions["1"] = ["Solid"]
+        #
+        # # The actual creatin of the things for the tilemap
+        # object_ary = {}
+        # prefix = "..//Start//"
+        # ppath = os.path.join("..//Sprites2//Yeee.png")
+        # tile_path = prefix + "TileMap.txt"
+        # collision_map = prefix + "CollisionMap.txt"
+        # object_map = prefix + "ObjectMap.txt"
+        # data_path = prefix + "//Data.txt"
+        # dta = "Yeee.png"
+        # tile_map = r.SpriteSheet()
+        # tile_map.load_texture("..//Sprites2//" + dta, cell_size=r.Vector2(48, 40))
+        # pokemon_center = r.TileMap(tile_map, collisions, tile_path, collision_map, object_map, object_ary, "bulb")
+        # nurse_joy.add_map(pokemon_center)
+        # nurse_joy.load(pokemon_center)
+
         self.world.add_obj(pc)
         self.world.add_obj(bulb)
         self.world.add_obj(moves)
+        # self.world.add_obj(nurse_joy)
 
     def get_engine(self):
         return self.engine
