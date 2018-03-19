451a06f70c66d05a1c7151b254a535c48bcb9435
classymonocle <mattjchang01@gmail.com>
2018-03-19 16:52:56 -0500

    Think I did the button selection
    What it says on the tin

--- Code/BattleUI.py
+++ Code/BattleUI.py
@@ -8,13 +8,21 @@ class BattleUI(r.GUIButton):
     def __init__(self):
         super_init_(self)
 
-    def clicked_action (self):
-        if mc.get_attack>slime.get_hp:
-            slime.set_hp(self, 0)
-        else:
-            slime.set_hp(self, slime.get_hp-mc.get_attack)
+    selected="fight"
 
-    # if function to detect which button and use different functions for each?
-    # In the future remove the self in the  parameter and replace with an actual texture
-    # Also find the texture
-    # Does the move up and down functions automatically select things or do we need to make that happen
+    def button_type(self):
+        self.current_index
+        if self.current_index==0:
+            selected="fight"
+        elif self.current_index==1:
+            selected="items"
+        elif self.current_index==2:
+            selected="run"
+
+
+    def clicked_action (selected):
+        if selected=="fight":
+            if mc.get_attack>slime.get_hp:
+                slime.set_hp(self, 0)
+            else:
+                slime.set_hp(self, slime.get_hp-mc.get_attack)
